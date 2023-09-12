e039af4ee4b80c3f58c6923e6dff3c79070728f2
LeinadRPFI <55161486+LeinadRPFI@users.noreply.github.com>
2023-09-12 15:49:47 -0300

    Robofriends basics
    Implementing the fundamentals of the robofriends project, the robots display (cards), the serchBox function and other things.

---
+++ robofriends/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Eduardeti Chielliti',
+        username: 'Eduardo37',
+        email: 'eduardoC@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ana Silva',
+        username: 'Aninha23',
+        email: 'aninha23@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'João Oliveira',
+        username: 'JotaOliveira',
+        email: 'jota.oliveira@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Maria Santos',
+        username: 'MarySantos',
+        email: 'mary.santos@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Carlos Pereira',
+        username: 'Carlitos',
+        email: 'carlitos@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Lúcia Fernandes',
+        username: 'LucinhaF',
+        email: 'lucinha.fernandes@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Rafaela Barbosa',
+        username: 'RafaB',
+        email: 'rafa.b@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Pedro Gomes',
+        username: 'PeGomes',
+        email: 'pedro.gomes@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Sara Costa',
+        username: 'SaraC',
+        email: 'sara.costa@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Lucas Fernandes',
+        username: 'LucasF',
+        email: 'lucas.fernandes@gmail.com'
+    }
+]
